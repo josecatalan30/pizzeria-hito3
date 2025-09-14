589407089c1ef2e74e9c0d356ff7a93d0566c54a
José Catalán <jcatalansolis@gmail.com>
2025-09-14 20:55:03 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pizzer-a_mamam-a/',
+  base: '/pizzeria-hito3/',
 })
